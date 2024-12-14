1e338d3a26632fd24d46f4a3068ebf030ce139d7
Farman Ali Ujjan <farman20ali@gmail.com>
2024-12-14 12:05:15 +0500

    fix script

--- script.sh
+++ script.sh
@@ -6,9 +6,8 @@ set -e
 # Set the default repository name
 DEFAULT_REPO_NAME="data-access"
 
-# Set the default repository name
+# Set the default DB IP
 DEFAULT_DB_IP="0.0.0.0"
-
 
 # Use the provided repository name or fall back to the default
 REPO_NAME=${1:-$DEFAULT_REPO_NAME}
@@ -19,8 +18,8 @@ REPO_URL="https://github.com/usamazahid/${REPO_NAME}"
 # Define the Docker image name
 IMAGE_NAME="quarkus/${REPO_NAME}-jvm"
 
-# Define the environment variable
-DB_IP=${2:-$DEFAULT_DB_IP}  # Replace with the actual DB IP
+# Use the provided DB IP or fall back to the default
+DB_IP=${2:-$DEFAULT_DB_IP}
 
 # Check if we're in the repository directory
 if [ "$(basename $PWD)" == "$REPO_NAME" ]; then
@@ -40,19 +39,20 @@ else
   fi
 fi
 
-# Export the environment variable for DB_IP
-export DB_IP=$DB_IP
-
 # Build the project
 echo "Building the project..."
-./mvnw package
+mvn package
 
 # Build the Docker image
 echo "Building the Docker image..."
 docker build -f src/main/docker/Dockerfile.jvm -t $IMAGE_NAME .
 
-# Run the Docker container in detached mode
-echo "Running the Docker container in the background..."
-docker run -d --rm -p 8080:8080 -e JAVA_DEBUG=true -e JAVA_DEBUG_PORT=*:5005 $IMAGE_NAME
+# Run the Docker container in detached mode, passing DB_IP as an environment variable
+echo "Running the Docker container in the background, exposing DB_IP..."
+docker run -d --rm -p 8080:8080 \
+  -e JAVA_DEBUG=true \
+  -e JAVA_DEBUG_PORT=*:5005 \
+  -e DB_IP=${DB_IP} \
+  $IMAGE_NAME
 
-echo "Container for '$IMAGE_NAME' is running in the background on port 8080."
+echo "Container for '$IMAGE_NAME' is running in the background on port 8080 with DB_IP='$DB_IP'."
